c47265b25b569401fab7689b44904bd440aaff83
Imelda Hope <imeldalarusso@gmail.com>
2024-09-30 19:43:25 +0300

    creates test-based routes

--- server/app.py
+++ server/app.py
@@ -6,3 +6,29 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return f'{param}'
+
+@app.route('/count/<int:param>')
+def count(param):
+    return '\n'.join(str(n) for n in range(param)) + '\n'
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)    
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        return str(num1 / num2)
+    elif operation == '%':
+        return str(num1 % num2)
